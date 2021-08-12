fd914a3b7a10dbbc229fb597866e2e457ed490e8
qingpingyin <qingpingyin@outlook.com>
2021-08-12 22:50:51 +0800

    update

--- service/cate/cate_service.go
+++ service/cate/cate_service.go
@@ -4,12 +4,10 @@ import (
 	"MI/models"
 )
 
-
-
 func CateTree(pid int)[]models.Categories{
 	categories := models.GetAllCate("parent_id=?", pid)
 	for i, v := range categories {
-		 if list,err := models.GetAllProductBy(1,7,"cid=?",int(v.Id));err == nil{
+		 if list,err := models.GetAllProductBy(1,6,"cid=?",int(v.Id));err == nil{
 		 	categories[i].Product =list
 		 }
 		categories[i].Children = CateTree(int(v.Id))
